23fd7bfca43bb74c0686100ca7b26f2a087de1f5
Gustavo Vázco <gustavovazco@gmail.com>
2026-01-08 22:08:25 -0600

    🚀 RELEASE: install v1.0.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,22 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+
   vite: {
     plugins: [
       // ...add your Vite plugins here
       tailwindcss(),
     ],
   },
+
   image: {
-    domains: ["coffeeshop.local"],
+    domains: ["coffeeshop.local", "coffeshop.core-hub-plex.space"],
   },
+
+  adapter: netlify(),
 });
