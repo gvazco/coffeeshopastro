190abb582e3a47354fa43e6a7cbbc4ee0711d6c6
Gustavo Vázco <gustavovazco@gmail.com>
2026-01-17 10:15:20 -0600

    tyr to rendering

--- dist/server/entry.mjs
+++ dist/server/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_BxOKJAtM.mjs';
-import { manifest } from './manifest_B27IiwPd.mjs';
+import { manifest } from './manifest_CFch6PXP.mjs';
 
 const serverIslandMap = new Map();;
 
