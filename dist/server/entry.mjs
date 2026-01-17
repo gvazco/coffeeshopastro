dc4772e823bc19d39ca380c91b26b860ff51efdd
Gustavo Vázco <gustavovazco@gmail.com>
2026-01-17 01:18:49 -0600

    🐛 FIX: update port entry

--- dist/server/entry.mjs
+++ dist/server/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_BxOKJAtM.mjs';
-import { manifest } from './manifest_DjLQoe8e.mjs';
+import { manifest } from './manifest_CYdZopQt.mjs';
 
 const serverIslandMap = new Map();;
 
