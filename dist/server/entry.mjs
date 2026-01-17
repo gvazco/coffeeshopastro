aa9d4ba139cd6a219c31beb6f52d8e8e4465b8f1
Gustavo Vázco <gustavovazco@gmail.com>
2026-01-17 01:09:07 -0600

    🐛 FIX: rollup gnu linux

--- dist/server/entry.mjs
+++ dist/server/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_BxOKJAtM.mjs';
-import { manifest } from './manifest_DpqdaBV2.mjs';
+import { manifest } from './manifest_D2dGEeyA.mjs';
 
 const serverIslandMap = new Map();;
 
